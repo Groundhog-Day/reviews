8f96dd9a11c3326f73268fec0e261e8f1de704ac
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-02-13 11:20:47 -0800

    Include new CRUD routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,15 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+//create
+app.post(
+  '/v1/api/:accommodationId/reviews',
+  (req, res) => {
+    
+  }
+);
+
+//read
 app.get(
   '/v1/api/:accommodationId/reviews',
   (req, res) => {
@@ -17,5 +26,19 @@ app.get(
     );
   }
 )
+
+//update
+app.patch('/v1/api/:accommodationId/reviews/:reviewId',
+  (req, res) => {
+    
+  }
+);
+
+//destroy
+app.delete('/v1/api/:accommodationId/reviews/:reviewId',
+  (req, res) => {
+    
+  }
+);
 
 app.listen(port, () => console.log(`Reviews server is listening on port ${port}!\n`))

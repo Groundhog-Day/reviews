5376b02705ec37dc3af56daf37cffd9a7ac039a4
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-02-22 15:58:51 -0800

    Fix error variable name in query callback

--- database/index.js
+++ database/index.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
 
-const connection = mysql.createConnection(mysqlConfig);goose.connect('mongodb://localhost/airbnbReviews', { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mysql.createConnection(mysqlConfig);
 
 const getAccommodation = function (callback, houseId) {
   connection.query('SELECT * from accommodations where id=' + houseId, function (error, accommodation, fields) {
-    if (err) {
-      console.log(err);
+    if (error) {
+      console.log(error);
     } else {
       results = accommodation[0];
 
       connection.query('SELECT * from reviews where accommodationId=' + houseId, function (error, reviews, fields) {
-        if (err) {
-          console.log(err);
+        if (error) {
+          console.log(error); 
         } else {
           results.reviews = reviews;
           callback(results);

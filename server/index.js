a5e567ed3643a4aea7969e03bca23684fb6bed05
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-02-24 14:36:06 -0800

    install k6

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
+require('newrelic');
 const express = require('express');
 const dbReviewModel = require('../database/index.js');
 const app = express();
 const port = 2020;
 const path = require('path');
 
-require('newrelic');
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 //create
 app.post(
-  '/v1/api/:accommodationId/reviews',
+  '/v1/api/reviews',
   (req, res) => {
-    
+    dbReviewModel.newReview(
+      (id1, id2) => {
+        console.log('New review posted\naccommodation id: ' +
+          id1 +
+          '\nreview id: ' + id2);
+
+        res.end();
+      },
+      req.params.accommodationId,
+      req.body
+    );
   }
 );
 
@@ -32,16 +41,27 @@ app.get(
 )
 
 //update
-app.patch('/v1/api/:accommodationId/reviews/:reviewId',
+app.patch('/v1/api/:reviewId/reviews',
   (req, res) => {
-    
+    dbReviewModel.updateReview(
+      () => { res.end() },
+      req.params.reviewId,
+      req.body
+    );
   }
 );
 
 //destroy
-app.delete('/v1/api/:accommodationId/reviews/:reviewId',
+app.delete('/v1/api/:reviewId/reviews',
   (req, res) => {
-    
+    dbReviewModel.deleteReview(
+      () => {
+        console.log('Review deleted\nreview id: ' + req.params.reviewId);
+
+        res.end();
+      },
+      req.params.reviewId
+    );
   }
 );
 
